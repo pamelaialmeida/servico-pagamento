1d5f285bd60e1d8e93fcba550a9e1d6ccdcf0ffb
Pâmela <pamelaialmeida@gmail.com>
2026-06-20 19:23:44 -0700

    ci: Adiciona pipeline ao projeto

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
 # servico-pagamento
 
 ## Orientações: Pré-requisitos:
+
 Crie uma classe que possua dois métodos: um para realizar pagamento e outro para consultar o último pagamento. Pagamentos serão armazenados como objetos Javascript dentro de uma lista de pagamentos. Cada pagamento terá as propriedades: Código de Barras, Empresa e Valor. Quando um pagamento for realizado e o valor for maior que 100.00, o pagamento também terá a propriedade categoria como 'cara', caso contrário, a propriedade categoria ficará como 'padrão'. O método de consultar trará apenas o último pagamento.
 
 ## Exemplo:
-ex. 
+
+ex.
 const servicoDePagamento = new ServicoDePagamento();
 servicoDePagamento.pagar('0987-7656-3475', 'Samar', 156.87);
 console.log(servicoDePagamento.consultarUltimoPagamento());
 {
-    codigoBarras: '0987-7656-3475',
-    empresa: 'Samar',
-    valor: 156.87,
-    categoria: 'cara'
+codigoBarras: '0987-7656-3475',
+empresa: 'Samar',
+valor: 156.87,
+categoria: 'cara'
 }
 
 ## Instruções para rodar:
+
 1. Clone o projeto para seu local
 2. Abra o projeto na IDE
 3. Estando na pasta do projeto, abra terminal e rode o comando "npm install"
@@ -24,7 +27,11 @@ console.log(servicoDePagamento.consultarUltimoPagamento());
 ## Estrutura do Projeto:
 
 ```bash
-servico-pagamento/                                  --> projeto
+servico-pagamento/                              --> projeto
+│
+├── .github/                                    --> diretório com configurações do github actions
+│   └── workflows                               --> diretório onde estão os arquivos de workflow
+│       └── pipeline.yml                        --> pipeline de CI
 │
 ├── mochawesome-report/                         --> diretório com relatório html gerado pelo mocha com os resultados dos testes
 │
@@ -32,10 +39,56 @@ servico-pagamento/                                  --> projeto
 │   └── servicoDePagamento.js                   --> arquivo com as funções solicitadas desenvolvidas
 │
 ├── test/                                       --> diretório com suites de testes
-│   ├── servicoDePagamento.test.js              --> arquivo com suíte de testes das funções do Serviço de pagamento
+│   └── servicoDePagamento.test.js              --> arquivo com suíte de testes das funções do Serviço de pagamento
 │
-├── .gitignore                                  --> arquivo com diretórios que devem ser ignorados pelo git e não enviados/rastreados 
-├── package-lock.json                           --> arquivo com versões das dependências instaladas/usadas no projeto 
+├── .gitignore                                  --> arquivo com diretórios que devem ser ignorados pelo git e não enviados/rastreados
+├── package-lock.json                           --> arquivo com versões das dependências instaladas/usadas no projeto
 ├── package.json                                --> arquivo com dependências do projeto, scripts e metadados do projeto
 ├── README.md                                   --> arquivo de documentação do projeto
 ```
+
+# Trabalho de conclusão da matéria de Integração Contínua para Automação de Testes:
+
+## Solução:
+
+A pipeline de CI (integração contínua) desenvolvida como solução deste trabalho foi configurada para rodar no Github actions como solicitado.
+Para isto, a seguinte estrutura de pastas foi adicionada ao projeto anteriormente desenvolvido: .github/workflows/pipeline.yml
+
+Na pipeline temos os eventos que disparam a execução da mesma, que ficam na sessão 'on', bem como cada tarefa/etapa a ser executada, que ficam na sessão 'jobs'. Esta não é uma self-hosted pipeline, visto que faz uso do próprio Github Actions para executar as etapas e rodar os testes.
+
+Como etapas, inclui a execução dos testes lint, sendo executados testes de formatação com prettier e testes de validação de qualidade de código com o eslint, e por fim a execução dos testes unitários, criados utilizando mocha, e a publicação do relatório gerado pelo mesmo.
+
+Como foi solicitado que utilizassemos o projeto desenvolvido na última matéria, esta pipeline não inclui testes feitos através de UI, portanto, não temos testes utilizando ferramentas como Playwright por exemplo, pois este projeto possui somente testes unitarios de código, sem validações via interface.
+
+## Conceitos utilizados:
+
+### Integração Contínua
+
+**Integração contínua (CI)** é a prática de desenvolvimento de software em que desenvolvedores frequentemente juntam suas alterações de código em um repositório central, onde são executadas em seguida inspeções e testes, com o objetivo de encontrar possíveis problemas de forma antecipada, reduzindo tempo de identificação e correção dos mesmos, se existirem.
+
+### Pipeline
+
+**Pipeline** é um processo, um fluxo a ser seguido. Resumidamente, para descrever a pipeline (ou seja, o fluxo/processo), precisamos criar um arquivo que irá descrever quais tarefas devem ser executadas, contendo para cada tarefa os passos e configurações necessários para sua execução, além dos eventos que irão disparar esta sequência de tarefas.
+
+### Gatilhos / Triggers
+
+**Gatilhos** definem os eventos que irão disparar o fluxo da pipeline. Conforme solicitado, para este projeto foram utilizados 3 possíveis gatilhos:
+
+1. <u>Execução manual</u> -> permitindo que a pipeline seja disparada de forma manual pela aba Actions no Github, acionada através de um clique de botão, usando o gatilho *workflow_dispatch*.
+
+2. <u>Execução por push</u> -> fazendo com que a pipeline seja disparada de forma automática a cada vez que um envio de código é realizado para a branch *master*.
+
+3. <u>Execução agendada (schedule)</u> -> fazendo com que a pipeline seja disparada de forma automática a cada 10 minutos, através do gatilho *schedule* com o tempo configurado em 'cron'.
+
+### Etapas / Tarefas / Jobs
+
+**Tarefas** definem cada fase que deve ser executada como parte do fluxo/processo.
+No caso da pipeline desenvolvida para este trabalho, a mesma é composta por dois jobs:
+
+1. <u>lint</u> -> tarefa que faz verificação da qualidade do código de forma geral, utilizando o prettier para verificar formatação do código e eslint para verificar boas práticas e possíveis erros. Pra isso, é utilizada uma máquina ubuntu, realizado clone do projeto, instalação do node, yarn e dependências do projeto e só então os testes são executados.
+
+2. <u>unit-tests</u> -> tarefa na qual os testes unitários desenvolvidos com mocha são executados e o relatório mochawesome gerado é publicado como artefato da execução, podendo ser acessado na aba Actions do Github. Pra isso, também é utilizada uma máquina ubuntu, realizado clone do projeto, instalação do node, yarn e dependências do projeto e só então os testes são executados. Esta tarefa só é executada caso a etapa de lint seja executada com sucesso.
+
+### Dependência entre Etapas / Tarefas / Jobs
+
+Como foi ensinado em aula a dependência entre jobs, resolvi implementar um exemplo neste trabalho. Para isso, utilizei o *needs* na tarefa de *unit-tests*, criando uma dependência desta tarefa com a de *lint*. Isso significa que o job de testes unitários só será executado se o job de lint passar com sucesso, garantindo a ordem de execução e evitando rodar etapas posteriores quando uma anterior já falhou.
--- test/servicoDePagamento.test.js
+++ test/servicoDePagamento.test.js
@@ -1,115 +1,194 @@
-import assert from 'node:assert';
-import ServicoDePagamento from '../src/servicoDePagamento.js';
+import assert from "node:assert";
+import ServicoDePagamento from "../src/servicoDePagamento.js";
 
-describe('Testes da funcionalidade de Serviço de Pagamento', () => {
+describe("Testes da funcionalidade de Serviço de Pagamento", () => {
+  let servicoDePagamento;
 
-    let servicoDePagamento;
+  beforeEach(() => {
+    servicoDePagamento = new ServicoDePagamento();
+  });
 
-    beforeEach(() => {
-        servicoDePagamento = new ServicoDePagamento();
+  it("TC01 - Deve retornar mensagem quando nenhum pagamento foi realizado e usuário tenta consultar o último pagamento", () => {
+    assert.equal(
+      servicoDePagamento.consultarUltimoPagamento(),
+      "Ainda não há pagamentos realizados.",
+    );
+  });
+
+  const dadosPagamento = [
+    {
+      codigoDeBarra: "0987-7656-3475",
+      empresa: "BC Hydro",
+      valorPagamento: 100.0,
+    },
+    {
+      codigoDeBarra: "0123-4567-8901",
+      empresa: "Telus",
+      valorPagamento: 99.99,
+    },
+    {
+      codigoDeBarra: "9876-5432-1098",
+      empresa: "Sanepar",
+      valorPagamento: 0.01,
+    },
+  ];
+
+  dadosPagamento.forEach(({ codigoDeBarra, empresa, valorPagamento }) => {
+    it(`TC02 - Deve realizar pagamento corretamente e setar categoria como padrão quando dados do pagamento informado são válidos e valor é igual a ${valorPagamento}`, () => {
+      servicoDePagamento.pagar(codigoDeBarra, empresa, valorPagamento);
+      const ultimoPagamento = servicoDePagamento.consultarUltimoPagamento();
+
+      assert.equal(ultimoPagamento.codigoBarras, codigoDeBarra);
+      assert.equal(ultimoPagamento.empresa, empresa);
+      assert.equal(ultimoPagamento.valor, valorPagamento);
+      assert.equal(ultimoPagamento.categoria, "padrão");
     });
+  });
 
-    it('TC01 - Deve retornar mensagem quando nenhum pagamento foi realizado e usuário tenta consultar o último pagamento', () => {
-        assert.equal(servicoDePagamento.consultarUltimoPagamento(), 'Ainda não há pagamentos realizados.');
-    });
+  it("TC03 - Deve realizar pagamento corretamente e setar categoria como caro quando dados do pagamento informado são válidos e valor é igual a 100.01", () => {
+    const dadosPagamento = {
+      codigoDeBarra: "0917-7654-3420",
+      empresa: "Copel",
+      valorPagamento: 100.01,
+    };
+    servicoDePagamento.pagar(
+      dadosPagamento.codigoDeBarra,
+      dadosPagamento.empresa,
+      dadosPagamento.valorPagamento,
+    );
 
-    const dadosPagamento = [
-        { codigoDeBarra: '0987-7656-3475', empresa: 'BC Hydro', valorPagamento: 100.00 },
-        { codigoDeBarra: '0123-4567-8901', empresa: 'Telus', valorPagamento: 99.99 },
-        { codigoDeBarra: '9876-5432-1098', empresa: 'Sanepar', valorPagamento: 0.01 }
-    ]
+    const ultimoPagamento = servicoDePagamento.consultarUltimoPagamento();
 
-    dadosPagamento.forEach(({ codigoDeBarra, empresa, valorPagamento }) => {
-        it(`TC02 - Deve realizar pagamento corretamente e setar categoria como padrão quando dados do pagamento informado são válidos e valor é igual a ${valorPagamento}`, () => {
+    assert.equal(ultimoPagamento.codigoBarras, dadosPagamento.codigoDeBarra);
+    assert.equal(ultimoPagamento.empresa, dadosPagamento.empresa);
+    assert.equal(ultimoPagamento.valor, dadosPagamento.valorPagamento);
+    assert.equal(ultimoPagamento.categoria, "cara");
+  });
+
+  it("TC04 - Deve apresentar mensagem quando código de barras informado já estiver pago", () => {
+    const dadosPagamento = {
+      codigoDeBarra: "0917-7654-3333",
+      empresa: "Best buy",
+      valorPagamento: 200.0,
+    };
+    servicoDePagamento.pagar(
+      dadosPagamento.codigoDeBarra,
+      dadosPagamento.empresa,
+      dadosPagamento.valorPagamento,
+    );
+
+    assert.throws(
+      function () {
+        servicoDePagamento.pagar(
+          dadosPagamento.codigoDeBarra,
+          dadosPagamento.empresa,
+          dadosPagamento.valorPagamento,
+        );
+      },
+      {
+        message:
+          "Já existe pagamento realizado para o Código de barras informado.",
+      },
+    );
+  });
+
+  const cenariosDadosFaltantes = [
+    {
+      codigoDeBarra: "",
+      empresa: "Telus",
+      valorPagamento: 180.54,
+      faltante: "Código de Barras",
+    },
+    {
+      codigoDeBarra: "9876-5432-1098",
+      empresa: "",
+      valorPagamento: 66.71,
+      faltante: "Empresa",
+    },
+    {
+      codigoDeBarra: "9876-5432-1098",
+      empresa: "Sanepar",
+      valorPagamento: null,
+      faltante: "Valor",
+    },
+  ];
+
+  cenariosDadosFaltantes.forEach(
+    ({ codigoDeBarra, empresa, valorPagamento, faltante }) => {
+      it(`TC05 - Deve apresentar mensagem de erro quando usuário tenta realizar pagamento sem informar dado obrigatório - Dado faltante ${faltante}`, () => {
+        assert.throws(
+          function () {
             servicoDePagamento.pagar(codigoDeBarra, empresa, valorPagamento);
-            const ultimoPagamento = servicoDePagamento.consultarUltimoPagamento();
-
-            assert.equal(ultimoPagamento.codigobarras, codigoDeBarra);
-            assert.equal(ultimoPagamento.empresa, empresa);
-            assert.equal(ultimoPagamento.valor, valorPagamento);
-            assert.equal(ultimoPagamento.categoria, 'padrão');
-        });
-    });
-
-    it('TC03 - Deve realizar pagamento corretamente e setar categoria como caro quando dados do pagamento informado são válidos e valor é igual a 100.01', () => {
-        const dadosPagamento = { codigoDeBarra: '0917-7654-3420', empresa: 'Copel', valorPagamento: 100.01 };
-        servicoDePagamento.pagar(dadosPagamento.codigoDeBarra, dadosPagamento.empresa, dadosPagamento.valorPagamento);
-
-        const ultimoPagamento = servicoDePagamento.consultarUltimoPagamento();
-
-        assert.equal(ultimoPagamento.codigobarras, dadosPagamento.codigoDeBarra);
-        assert.equal(ultimoPagamento.empresa, dadosPagamento.empresa);
-        assert.equal(ultimoPagamento.valor, dadosPagamento.valorPagamento);
-        assert.equal(ultimoPagamento.categoria, 'cara');
-    });
-
-    it('TC04 - Deve apresentar mensagem quando código de barras informado já estiver pago', () => {
-        const dadosPagamento = { codigoDeBarra: '0917-7654-3333', empresa: 'Best buy', valorPagamento: 200.00 };
-        servicoDePagamento.pagar(dadosPagamento.codigoDeBarra, dadosPagamento.empresa, dadosPagamento.valorPagamento);
-
-        assert.throws(
-            function () { servicoDePagamento.pagar(dadosPagamento.codigoDeBarra, dadosPagamento.empresa, dadosPagamento.valorPagamento) },
-            { message: 'Já existe pagamento realizado para o Código de barras informado.' }
+          },
+          { message: "Informar todos os dados necessários para pagamento." },
         );
-    });
+      });
+    },
+  );
 
-    const cenariosDadosFaltantes = [
-        { codigoDeBarra: '', empresa: 'Telus', valorPagamento: 180.54, faltante: 'Código de Barras' },
-        { codigoDeBarra: '9876-5432-1098', empresa: '', valorPagamento: 66.71, faltante: 'Empresa' },
-        { codigoDeBarra: '9876-5432-1098', empresa: 'Sanepar', valorPagamento: null, faltante: 'Valor' }
-    ]
+  const cenariosDadosTipoInvalido = [
+    {
+      codigoDeBarra: 1234567890,
+      empresa: "Apple",
+      valorPagamento: 2080.21,
+      invalido: "Código de Barras",
+    },
+    {
+      codigoDeBarra: "3476-1232-2158",
+      empresa: 9876543210,
+      valorPagamento: 325.8,
+      invalido: "Empresa",
+    },
+    {
+      codigoDeBarra: "9116-5778-1365",
+      empresa: "H&M",
+      valorPagamento: "123.20d",
+      invalido: "Valor",
+    },
+  ];
 
-    cenariosDadosFaltantes.forEach(({ codigoDeBarra, empresa, valorPagamento, faltante }) => {
-        it(`TC05 - Deve apresentar mensagem de erro quando usuário tenta realizar pagamento sem informar dado obrigatório - Dado faltante ${faltante}`, () => {
-            assert.throws(
-                function () { servicoDePagamento.pagar(codigoDeBarra, empresa, valorPagamento) },
-                { message: 'Informar todos os dados necessários para pagamento.' }
-            );
-
-        });
-    });
-
-    const cenariosDadosTipoInvalido = [
-        { codigoDeBarra: 1234567890, empresa: 'Apple', valorPagamento: 2080.21, invalido: 'Código de Barras' },
-        { codigoDeBarra: '3476-1232-2158', empresa: 9876543210, valorPagamento: 325.80, invalido: 'Empresa' },
-        { codigoDeBarra: '9116-5778-1365', empresa: 'H&M', valorPagamento: '123.20d', invalido: 'Valor' }
-    ]
-
-    cenariosDadosTipoInvalido.forEach(({ codigoDeBarra, empresa, valorPagamento, invalido }) => {
-        it(`TC06 - Deve apresentar mensagem de erro quando usuário tenta realizar pagamento sem informar dado obrigatório - Dado inválido ${invalido}`, () => {
-            assert.throws(
-                function () { servicoDePagamento.pagar(codigoDeBarra, empresa, valorPagamento) },
-                { message: 'Dados informados estão inválidos.' }
-            );
-        });
-    });
-
-    it('TC07 - Deve apresentar mensagem quando valor informado for igual a 0.00', () => {
+  cenariosDadosTipoInvalido.forEach(
+    ({ codigoDeBarra, empresa, valorPagamento, invalido }) => {
+      it(`TC06 - Deve apresentar mensagem de erro quando usuário tenta realizar pagamento sem informar dado obrigatório - Dado inválido ${invalido}`, () => {
         assert.throws(
-            function () { servicoDePagamento.pagar('5620-5687-3020', 'Sephora', 0.00) },
-            { message: 'Valor informado deve ser um número maior ou igual a zero.' }
+          function () {
+            servicoDePagamento.pagar(codigoDeBarra, empresa, valorPagamento);
+          },
+          { message: "Dados informados estão inválidos." },
         );
-    });
+      });
+    },
+  );
 
-    it('TC08 - Deve apresentar mensagem quando valor informado for negativo', () => {
-        assert.throws(
-            function () { servicoDePagamento.pagar('1122-3344-5566', 'Walmart', -120.32) },
-            { message: 'Valor informado deve ser um número maior ou igual a zero.' }
-        );
-    });
+  it("TC07 - Deve apresentar mensagem quando valor informado for igual a 0.00", () => {
+    assert.throws(
+      function () {
+        servicoDePagamento.pagar("5620-5687-3020", "Sephora", 0.0);
+      },
+      { message: "Valor informado deve ser um número maior ou igual a zero." },
+    );
+  });
 
-    it('TC09 - Deve retornar último pagamento realizado corretamente quando consulta de último pagamento é realizada', () => {
-        servicoDePagamento.pagar('1111-1111-1111', 'Copel', 120.44);
-        servicoDePagamento.pagar('2222-2222-2222', 'Mastercard', 500.39);
-        servicoDePagamento.pagar('3333-3333-3333', 'Sanepar', 44.20);
-        servicoDePagamento.pagar('4444-4444-4444', 'Claro', 70.82);
+  it("TC08 - Deve apresentar mensagem quando valor informado for negativo", () => {
+    assert.throws(
+      function () {
+        servicoDePagamento.pagar("1122-3344-5566", "Walmart", -120.32);
+      },
+      { message: "Valor informado deve ser um número maior ou igual a zero." },
+    );
+  });
 
-        const ultimoPagamento = servicoDePagamento.consultarUltimoPagamento();
+  it("TC09 - Deve retornar último pagamento realizado corretamente quando consulta de último pagamento é realizada", () => {
+    servicoDePagamento.pagar("1111-1111-1111", "Copel", 120.44);
+    servicoDePagamento.pagar("2222-2222-2222", "Mastercard", 500.39);
+    servicoDePagamento.pagar("3333-3333-3333", "Sanepar", 44.2);
+    servicoDePagamento.pagar("4444-4444-4444", "Claro", 70.82);
 
-        assert.equal(ultimoPagamento.codigobarras, '4444-4444-4444');
-        assert.equal(ultimoPagamento.empresa, 'Claro');
-        assert.equal(ultimoPagamento.valor, 70.82);
-        assert.equal(ultimoPagamento.categoria, 'padrão');
-    });
+    const ultimoPagamento = servicoDePagamento.consultarUltimoPagamento();
 
+    assert.equal(ultimoPagamento.codigoBarras, "4444-4444-4444");
+    assert.equal(ultimoPagamento.empresa, "Claro");
+    assert.equal(ultimoPagamento.valor, 70.82);
+    assert.equal(ultimoPagamento.categoria, "padrão");
+  });
 });
